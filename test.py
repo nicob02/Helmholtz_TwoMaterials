6c467477dc0d4064ad9b834b891e45ec59a69e8e
nicob02 <66340166+nicob02@users.noreply.github.com>
2025-04-21 19:05:19 -0400

    Update test.py

--- test.py
+++ test.py
@@ -59,8 +59,9 @@ print('************* model test starts! ***********************')
 predicted_results = modelTester(test_config)
 
 u_exact = func_main.exact_solution(graph)  
+u_exact_np  = u_exact_t.detach().cpu().numpy()
 # 2) Compute exact & error
-rel_l2 = compute_steady_error(predicted_results, u_exact, test_config)
+rel_l2 = compute_steady_error(predicted_results, u_exact_np, test_config)
 print(f"Relative L2 error: {rel_l2:.3e}")
 
 # 3) Render the three‐panel result
