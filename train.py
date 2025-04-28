08d5f806e620fb6d34e4278ecc60fd70e6025b13
nicob02 <66340166+nicob02@users.noreply.github.com>
2025-04-28 14:17:54 -0400

    Update train.py

--- train.py
+++ train.py
@@ -81,16 +81,15 @@ setattr(train_config, 'graph', graph)
 setattr(train_config, 'model', model)
 setattr(train_config, 'optimizer', optimizer)
 setattr(train_config, 'train_steps', 1)    # 1 train step, extend this in the future to a dynamic source function that changes with time.
-setattr(train_config, 'epchoes', 20000)
+setattr(train_config, 'epchoes', 10)
 setattr(train_config, 'NodeTypesRef', ElectrodeMesh.node_type_ref) 
 setattr(train_config, 'step_times', 1)
 setattr(train_config, 'ndim', out_ndim)
 setattr(train_config, 'lrstep', 100) #learning rate decay epchoes
 setattr(train_config, 'writer', writer)
 setattr(train_config, 'func_main', func_main)
-setattr(train_config, 'lambda_neu', 1.0) 
-setattr(train_config, 'lambda_if', 1.0) 
-setattr(train_config, 'lambda_dir', 1.0) 
+setattr(train_config, 'lambda_neu', 15.0) 
+setattr(train_config, 'lambda_if', 100.0) 
 
 modelTrainer(train_config)
 
