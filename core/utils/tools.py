49106ccce92a63c9d1132200287b14c33f618bf8
nicob02 <66340166+nicob02@users.noreply.github.com>
2025-06-11 11:01:28 -0400

    Update tools.py

--- core/utils/tools.py
+++ core/utils/tools.py
@@ -88,7 +88,7 @@ def modelTrainer(config):
     M_if  = interface.sum().float()   # number of interface points
     M_neu  = bottom.sum().float() + top.sum().float()      # number of Neu points
 
-    λ_if  = 1e7
+    λ_if  = 1e6
     λ_neu = 1
     # 4) training loop
     for epoch in range(1, config.epchoes+1):
